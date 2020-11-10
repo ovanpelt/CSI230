d43906ec8c227f4cacc761b2c8b598926e3ecf10
Olivia Van Pelt <oliviavanpelt@pop-os.localdomain>
2020-11-10 14:51:26 -0500

    creating inner folders

---
+++ week10/src/week10.cpp
@@ -0,0 +1,20 @@
+/**
+* @file week10.cpp
+* @brief Week 10, c++ review and warmup from CSI140
+*
+* @author Bilbo Baggins
+* @assignment Lecture 10.1
+* @date 11/8/2020
+* credits
+*
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//in
+	return EXIT_SUCCESS
+}
